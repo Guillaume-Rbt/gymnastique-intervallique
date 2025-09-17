4b974a71428d7b4ebb8b778a257ad7eded921dc7
Guillaume <g.robert@onlineformapro.com>
2025-09-17 14:57:55 +0200

    initial commit add  dependences and clean

---
+++ uno.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, presetWind4 } from 'unocss'
+
+export default defineConfig({
+    presets: [presetWind4()],
+    content: {
+        pipeline: {
+            include: ["src/**/*.tsx"],
+        },
+    },
+
+    theme: {
+        breakpoint: {
+            xs: "480px",
+            sm: "576px",
+            md: "768px",
+            lg: "992px",
+            xl: "1200px",
+            xxl: "1400px",
+        },
+        colors: {
+            interactable: "#fc9026",
+        },
+    },
+})
