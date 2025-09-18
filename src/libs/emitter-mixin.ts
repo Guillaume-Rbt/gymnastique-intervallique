4fe0f735d858763c43db79adc8812642f35712a4
Guillaume <g.robert@onlineformapro.com>
2025-09-18 13:02:27 +0200

    add utils

---
+++ src/libs/emitter-mixin.ts
@@ -0,0 +1,72 @@
+
+
+type Listener = (...args: any[]) => void;
+
+export default class Emitter {
+    private listeners: { [event: string]: Listener[] };
+
+    constructor() {
+        this.listeners = {};
+        this.addListener = this.addListener.bind(this);
+        this.on = this.on.bind(this);
+        this.emit = this.emit.bind(this);
+        this.removeListener = this.removeListener.bind(this);
+        this.off = this.off.bind(this);
+        this.once = this.once.bind(this);
+        this.listernerCount = this.listernerCount.bind(this);
+        this.rawListeners = this.rawListeners.bind(this);
+    }
+
+    addListener(event: string, fn: Listener): this {
+        this.listeners[event] = this.listeners[event] || [];
+        this.listeners[event].push(fn);
+        return this;
+    }
+
+    on(event: string, fn: Listener): this {
+        return this.addListener(event, fn);
+    }
+
+    emit(event: string, data?: Record<string, any>): this {
+        const lis = this.listeners[event] || [];
+
+        lis.forEach((fn: Listener) => {
+            fn({ eventName: event, ...data });
+        });
+
+        return this;
+    }
+
+    removeListener(event: string, fn: Listener): this {
+        let lis = this.listeners[event] || [];
+
+        for (let i = lis.length - 1; i >= 0; i--) {
+            if (lis[i] === fn) {
+                lis.splice(i, 1);
+                break;
+            }
+        }
+        return this;
+    }
+
+    off(event: string, fn: Listener): this {
+        return this.removeListener(event, fn);
+    }
+
+    once(event: string, fn: Listener): this {
+        const onceWrapper = (data?: Record<string, any>) => {
+            fn(data);
+            this.off(event, onceWrapper);
+        };
+        return this.addListener(event, onceWrapper);
+    }
+
+    listernerCount(event: string): number {
+        let fns = this.listeners[event] || [];
+        return fns.length;
+    }
+
+    rawListeners(event: string): Listener[] {
+        return this.listeners[event] || [];
+    }
+}
---
+++ src/utils/constants.ts
@@ -0,0 +1,65 @@
+export const intervals = [
+    'unisson',
+    'seconde mineure',
+    'seconde majeure',
+    'tierce mineure',
+    'tierce majeure',
+    'quarte juste',
+    'quarte aug / quinte dim',
+    'quinte juste',
+    'quinte aug / sixte mineure',
+    'sixte majeure / septième dim',
+    'septième mineure',
+    'septième majeure',
+    'octave',
+    'neuvième'
+]
+
+export const buttons = [
+    'Unisson',
+    'Seconde mineure',
+    'Seconde Majeure',
+    'Tierce mineure',
+    'Tierce Majeure',
+    'Quarte juste',
+    'Quarte aug&nbsp/ Quinte dim',
+    'Quinte juste',
+    'Quinte aug&nbsp/ Sixte mineure',
+    'Sixte majeure&nbsp/ Septième dim',
+    'Septième mineure',
+    'Septième Majeure',
+    'Octave',
+    'Neuvième mineure'
+]
+
+export const buttonsMobile = [
+    'Uniss.',
+    '2<sup>nde</sup> m',
+    '2nde M',
+    '3<sup>ce</sup> m',
+    '3<sup>ce</sup> M',
+    '4<sup>te</sup>',
+    '4<sup>te</sup> aug / 5<sup>te</sup> dim',
+    '5<sup>te</sup>',
+    '5<sup>te</sup> aug / 6<sup>te</sup> m',
+    '6<sup>te</sup> M / 7<sup>ème</sup> dim',
+    '7<sup>ème</sup> m',
+    '7<sup>ème</sup> M',
+    '8<sup>ve</sup>',
+    '9<sup>ème</sup> m'
+]
+
+export const notes = [
+    'C',
+    ['C#', 'Db'],
+    'D',
+    ['D#', 'Eb'],
+    ['E', 'Fb'],
+    'F',
+    ['F#', 'Gb'],
+    'G',
+    ['G#', 'Ab'],
+    'A',
+    ['A#', 'Bb'],
+    ['B', 'Cb'],
+]
