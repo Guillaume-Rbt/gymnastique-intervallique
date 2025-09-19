6fd38b57c71be63ef580b586d7dd0c0d13a772a1
Guillaume <robert.guillaume70@laposte.net>
2025-09-19 23:51:24 +0200

    make intervalGenerator and begin game class

---
+++ src/libs/game.ts
@@ -0,0 +1,87 @@
+import Emitter from "./emitter-mixin";
+import { Interval } from "./interval-generator";
+import RandomIntervalGenerator from "./interval-generator";
+import { intervals } from "../utils/constants";
+
+export enum GAME_STATES {
+    INIT = "init",
+    READY = "ready",
+    STARTED = "started",
+    WAIT_ANSWER = "wait",
+    ANSWERED = "answered",
+    INTERVAL_PLAYED = "interval.played",
+    ENDED = "ended",
+}
+
+type gameConfig = { allowedIntervals: Map<number, { text: string; enabled: boolean }> }
+
+export default class Game extends Emitter {
+
+    config: gameConfig = { allowedIntervals: new Map() }
+    intervals: Interval[] = [];
+    allowedIntervals: Map<number, { text: string; enabled: boolean }> = new Map();
+    intervalsGenerator: RandomIntervalGenerator = new RandomIntervalGenerator();
+    currentIntervalIndex: number = 0;
+    #state: GAME_STATES = GAME_STATES.INIT;
+
+
+
+    static get STATES() {
+        return GAME_STATES;
+    }
+
+    get state() {
+        return this.#state;
+    }
+
+    set state(value: GAME_STATES) {
+        this.#state = value;
+        this.emit(Game.EVENTS.STATE_CHANGED, { state: this.#state });
+    }
+
+    static EVENTS =
+        {
+            STATE_CHANGED: "game.state.changed",
+            PROGRESS_CHANGED: "game.progress.changed",
+        }
+
+    constructor(options: Partial<gameConfig> = {}) {
+        super();
+
+        this.config = {
+            allowedIntervals: new Map(
+                intervals.map((interval, index) => {
+                    return [index, { text: interval, enabled: true }];
+                })),
+            ...options,
+        };
+
+        this.allowedIntervals = this.config.allowedIntervals;
+    }
+
+    startGame() {
+        this.currentIntervalIndex = 0;
+        this.state = GAME_STATES.STARTED;
+    }
+
+    getCurrentInterval() {
+        return this.intervals[this.currentIntervalIndex];
+    }
+
+    nextInterval() {
+
+        if (this.currentIntervalIndex < this.intervals.length - 1) {
+
+            this.currentIntervalIndex++;
+            this.emit(Game.EVENTS.PROGRESS_CHANGED, { current: this.currentIntervalIndex, total: this.intervals.length });
+            return true;
+        }
+        return false;
+    }
+
+    checkAnswer(answer: string) {
+        const currentInterval = this.getCurrentInterval();
+        if (!currentInterval) return false;
+        return currentInterval.name === answer;
+    }
+}
---
+++ src/libs/interval-generator.ts
@@ -0,0 +1,165 @@
+import { intervals, notes } from "../utils/constants";
+import { randomNumber } from "../utils/numbers";
+
+export type allowedIntervalsType = Map<number, { text: string | string[], enabled: boolean; }>;
+type intervalGeneratorOptions = {
+  allowedIntervals: allowedIntervalsType;
+};
+
+export default class RandomIntervalGenerator {
+  options: intervalGeneratorOptions;
+  intervalsKey: number[] = [];
+  #allowedIntervals!: allowedIntervalsType;
+
+  set allowedIntervals(value: allowedIntervalsType) {
+    this.#allowedIntervals = value;
+    this.intervalsKey = Array.from(this.allowedIntervals.keys()).filter((key) => {
+      if (this.allowedIntervals.get(key)!.enabled) {
+        return this.allowedIntervals.get(key)?.enabled;
+      }
+    })
+  }
+
+  get allowedIntervals(): allowedIntervalsType {
+    return this.#allowedIntervals;
+  }
+
+  constructor(options: Partial<intervalGeneratorOptions> = {}) {
+    this.setDefaultOptions = this.setDefaultOptions.bind(this);
+    this.generateInterval = this.generateInterval.bind(this);
+    this.adjustIntervalOctave = this.adjustIntervalOctave.bind(this);
+
+    this.options = this.setDefaultOptions(options);
+
+    this.generateAnyIntervals = this.generateAnyIntervals.bind(this);
+
+    this.allowedIntervals = this.options.allowedIntervals;
+
+
+  }
+
+  setDefaultOptions(options: Partial<intervalGeneratorOptions>) {
+    const mergeOptions = {
+      allowedIntervals: new Map(
+        intervals.map((interval, index) => {
+          return [index, { text: interval, enabled: true }];
+        }),
+      ),
+      ...options,
+    };
+
+    return mergeOptions;
+  }
+
+  generateInterval() {
+    const index = randomNumber(0, this.intervalsKey.length - 1);
+    const length = this.intervalsKey[index];
+    const name = this.allowedIntervals.get(length)!.text as string;
+
+    return new Interval({ length, name });
+  }
+
+  adjustIntervalOctave(interval: Interval) {
+    switch (interval.direction) {
+      case "asc":
+        if (interval.endNote.octave > 2) {
+          interval.startNote.octave = interval.startNote.octave - 1;
+          interval.endNote.octave = interval.endNote.octave - 1;
+        }
+        break;
+      case "desc":
+        if (interval.endNote.octave < 0) {
+          interval.startNote.octave = interval.startNote.octave + 1;
+          interval.endNote.octave = interval.endNote.octave + 1;
+        }
+        break;
+    }
+    return interval;
+  }
+
+  generateAnyIntervals(nbIntervals: number = 10) {
+    let intervals = [];
+    for (let i = 0; i < nbIntervals; i++) {
+      intervals.push(this.generateInterval());
+    }
+    return intervals;
+  }
+}
+
+class Note {
+  name: string | string[];
+  octave: number;
+  index: number;
+
+  constructor(index: number, octave: number | null = null) {
+    this.index = index;
+    this.name = notes[index];
+    this.octave = octave !== null ? octave : randomNumber(0, 2);
+  }
+}
+
+export class Interval {
+  length: number;
+  direction: string;
+  name: string;
+  isPassOctave: boolean = false;
+  startNote: Note;
+  endNote: Note;
+
+  #notes: { startNote: Note; endNote: Note };
+
+  constructor(data: { length: number; name: string }) {
+    this.length = data.length;
+    this.name = data.name;
+    this.direction = randomNumber(0, 1) == 0 ? "desc" : "asc";
+    this.#notes = this.setNotes();
+
+    this.startNote = this.#notes.startNote;
+    this.endNote = this.#notes.endNote;
+
+    this.adjustlOctave = this.adjustlOctave.bind(this);
+
+
+  }
+
+  adjustlOctave({ startNote, endNote }: { startNote: Note; endNote: Note }) {
+    switch (this.direction) {
+      case "asc":
+        if (endNote.octave > 2) {
+          startNote.octave = startNote.octave - 1;
+          endNote.octave = endNote.octave - 1;
+        }
+        break;
+      case "desc":
+        if (endNote.octave < 0) {
+          startNote.octave = startNote.octave + 1;
+          endNote.octave = endNote.octave + 1;
+        }
+        break;
+    }
+
+    return { startNote: startNote, endNote: endNote };
+  }
+
+  setNotes() {
+    const startNote = new Note(randomNumber(0, 11));
+
+    this.isPassOctave =
+      this.direction == "asc" ? (startNote.index + this.length > 11 ? true : false) : startNote.index - this.length < 0 ? true : false;
+
+    const endNoteIndex =
+      this.direction == "asc"
+        ? startNote.index + this.length > 11
+          ? startNote.index + this.length - 12
+          : startNote.index + this.length
+        : startNote.index - this.length < 0
+          ? 12 + (startNote.index - this.length)
+          : startNote.index - this.length;
+
+    const endNote = new Note(
+      endNoteIndex,
+      this.isPassOctave ? (this.direction == "asc" ? startNote.octave + 1 : startNote.octave - 1) : startNote.octave,
+    );
+    return this.adjustlOctave({ startNote, endNote });
+  }
+}
